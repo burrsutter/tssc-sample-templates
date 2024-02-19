1cd39d5f1f78b4da82c34abc1e9d4da1bef7021d
Burr Sutter <burrsutter@gmail.com>
2024-02-19 19:24:07 -0300

    adding script

---
+++ prod-to-demo.sh
@@ -0,0 +1,18 @@
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
+
+GITSERVER=gitlab-gitlab.apps.cluster-79hq7.sandbox2323.opentlc.com
+QUAYSERVER=quay-79hq7.apps.cluster-79hq7.sandbox2323.opentlc.com
+for template in $SCRIPTDIR/../templates/*
+do
+    template=$(realpath $template)
+    if [ -d "$template" ]; then
+        echo $template
+        cd $template
+        sed -i s!quay.io/redhat-appstudio!QUAY-IMAGE_REF!g  template.yaml 
+        sed -i s!quay.io!$QUAYSERVER!g  template.yaml 
+        sed -i s!QUAY-IMAGE_REF!quay.io/redhat-appstudio!g  template.yaml 
+        sed -i s!gitlab.com!$GITSERVER!g  template.yaml  
+        sed -i 's/argoInstance\: default/argoInstance\: main/g'  template.yaml 
+        sed -i 's/namespace: ${{ parameters.namespace}}/namespace: janus-argocd/g'  template.yaml 
+    fi
+done
